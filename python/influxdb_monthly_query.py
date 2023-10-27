3883705932f53197c8fa0aa58b53713d83972e20
airsalvatierra <air.salvatierra@gmail.com>
2023-10-26 18:39:30 -0600

    general updates

--- python/influxdb_monthly_query.py
+++ python/influxdb_monthly_query.py
@@ -1,9 +1,10 @@
 import influxdb_client
+import os
+
 import pandas as pd
 import pyarrow as pa
 import pyarrow.parquet as pq
-from datetime import datetime
-import os
+from datetime import datetime, timedelta
 from dotenv import load_dotenv
 
 load_dotenv()  # take environment variables from .env.
@@ -13,6 +14,15 @@ ORGANIZATION = os.environ.get('INFLUXDB_ORGANIZATION')
 TOKEN = os.environ.get('INFLUXDB_TOKEN')
 BASE_URL=os.environ.get('INFLUXDB_URL')
 PARQUET_FILES_DIRECTORY=os.environ.get('PARQUET_FILES_DIRECTORY')
+START_DATETIME=os.environ.get('START_DATETIME')
+END_DATETIME=os.environ.get('END_DATETIME')
+
+today = datetime.now()
+yesterday = today - timedelta(days=1)
+
+if not START_DATETIME or not END_DATETIME:
+    START_DATETIME = f'{yesterday.strftime("%Y-%m-%dT")}00:00:00Z'
+    END_DATETIME= f'{yesterday.strftime("%Y-%m-%dT")}23:59:00Z'
 
 
 client = influxdb_client.InfluxDBClient(
@@ -23,19 +33,26 @@ client = influxdb_client.InfluxDBClient(
 
 # Query script
 query_api = client.query_api()
-servers_main_query = '''
+servers_main_query = f'''
 from(bucket: "server")
-    |> range(start: -5m, stop: now())
+    |> range(start: {START_DATETIME}, stop: {END_DATETIME})
     |> filter(fn: (r) => r["_measurement"] == "cpu" or r["_measurement"] == "disk" or r["_measurement"] == "diskio" or r["_measurement"] == "kernel" or r["_measurement"] == "mem" or r["_measurement"] == "net" or r["_measurement"] == "netstat" or r["_measurement"] == "processes" or r["_measurement"] == "swap" or r["_measurement"] == "system")
     |> yield(name: "mean")
 '''
 
-print('Getting data from InfluxDB!')
-print('start: ', datetime.now())
+print(f'Getting data from InfluxDB for date: {yesterday.strftime("%Y-%m-%d")}!')
+start_process = datetime.now()
+print('start: ', start_process)
+
 result = query_api.query(org=ORGANIZATION, query=servers_main_query)
 
 # Assuming you want to convert the result to a Pandas DataFrame
-print('end: ', datetime.now())
+end_process = datetime.now()
+print('end: ', end_process)
+
+process_time = end_process - start_process
+print(f'The query took: {process_time}')
+
 data = []
 for index, table in enumerate(result):
     if index == 0:
@@ -51,8 +68,7 @@ df = pd.DataFrame(data, columns=columns)
 new_table = pa.Table.from_pandas(df)
 
 # Specify the file path
-current_date = datetime.now()
-current_year_month = current_date.strftime('%Y%m')
+current_year_month = yesterday.strftime('%Y%m')
 parquet_file = f'{current_year_month}_servers.parquet'
 
 # Write the Arrow Table to a Parquet file
